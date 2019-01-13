d15d077e1393e9cb9e122011bf1c198d980009cc
newcat <freddy.wagner@web.de>
2019-01-13 19:12:32 +0100

    Initial project setup

---
+++ src/renderer/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <ul>
+        <li
+            v-for="item in items"
+            :key="item"
+        >{{ item }}</li>
+    </ul>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class  extends Vue {
+
+    items = [ "Item 1", "Item 2", "Item 3" ];
+
+}
+</script>
