9bf7a87f7cf48ad7330e2e160ce0315c4a3e7ead
Frederik Wagner <freddy.wagner@web.de>
2019-02-17 23:17:24 +0100

    Added BaklavaJS

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,19 +1,17 @@
 <template>
-    <ul>
-        <li
-            v-for="item in items"
-            :key="item"
-        >{{ item }}</li>
-    </ul>
+    <div style="width:100%;height:90vh;">
+        <baklava-editor :model="editor"></baklava-editor>
+    </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
+import { Editor } from "baklavajs";
 
 @Component
 export default class  extends Vue {
 
-    items = [ "Item 1", "Item 2", "Item 3" ];
+    editor = new Editor();
 
 }
 </script>
--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -3,6 +3,10 @@ import App from "./App.vue";
 
 Vue.config.productionTip = false;
 
+import Baklava from "baklavajs";
+Vue.use(Baklava);
+import "baklavajs/dist/styles/all.scss";
+
 new Vue({
     render: (h) => h(App)
 }).$mount("#app");
