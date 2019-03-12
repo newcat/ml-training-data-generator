3dd3dbdc0c670125703a54b8510751b62c12f39f
newcat <freddy.wagner@web.de>
2019-03-12 23:46:48 +0100

    Function node done

--- src/createEditor.ts
+++ src/createEditor.ts
@@ -4,6 +4,7 @@ import UniformNode from "./nodes/random/UniformNode";
 import NormalNode from "./nodes/random/NormalNode";
 import ExponentialNode from "./nodes/random/ExponentialNode";
 import OutputNode from "./nodes/OutputNode";
+import FunctionNode from "./nodes/function/FunctionNode";
 import * as ValueNodes from "./nodes/ValueNodes";
 
 export default function createEditor(): Editor {
@@ -16,16 +17,24 @@ export default function createEditor(): Editor {
     editor.registerNodeType("UniformNode", UniformNode, "Random");
     editor.registerNodeType("NormalNode", NormalNode, "Random");
     editor.registerNodeType("ExponentialNode", ExponentialNode, "Random");
+    editor.registerNodeType("FunctionNode", FunctionNode);
     editor.registerNodeType("OutputNode", OutputNode);
     editor.nodeInterfaceTypes
         .addType("number", "cyan")
         .addType("string", "white")
         .addType("boolean", "lightgreen")
+        .addType("any", "black")
         .addConversion("number", "string", String)
         .addConversion("number", "boolean", (v) => !!v)
+        .addConversion("number", "any", (v) => v)
         .addConversion("string", "number", parseFloat)
+        .addConversion("string", "any", (v) => v)
         .addConversion("boolean", "number", (v) => v ? 1 : 0)
-        .addConversion("boolean", "string", String);
+        .addConversion("boolean", "string", String)
+        .addConversion("boolean", "any", (v) => v)
+        .addConversion("any", "number", parseFloat)
+        .addConversion("any", "string", String)
+        .addConversion("any", "boolean", Boolean);
 
     return editor;
 
---
+++ src/nodes/function/CodeOption.vue
@@ -0,0 +1,66 @@
+<template>
+<div>
+    <h3>Inputs</h3>
+    <interface-view
+        v-for="(intf, iname) in node.inputInterfaces" :key="iname" :name="iname" class="mt-3"
+        @remove="remove(iname)" @rename="renameInput(iname, $event)"
+    >
+    </interface-view>
+    <button @click="addInput" class="dark-button mt-3 w-100">Add Input</button>
+
+    <h3 class="mt-3">Function</h3>
+    <code-editor :value="value" @input="$emit('input', $event)"></code-editor>
+
+    <h3 class="mt-3">Outputs</h3>
+    <interface-view
+        v-for="(intf, iname) in node.outputInterfaces" :key="iname" :name="iname" class="mt-3"
+        @remove="remove(iname)" @rename="renameOutput(iname, $event)"
+    >
+    </interface-view>
+    <button @click="addOutput" class="dark-button mt-3 w-100">Add Output</button>
+
+</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import FunctionNode from "./FunctionNode";
+import InterfaceView from "./InterfaceView.vue";
+import CodeEditor from "./CodeEditor.vue";
+
+@Component({
+    components: { InterfaceView, CodeEditor }
+})
+export default class CodeOption extends Vue {
+
+    @Prop()
+    node!: FunctionNode;
+
+    @Prop()
+    value!: string;
+
+    addInput() {
+        this.node.addInput("Input" + Object.keys(this.node.inputInterfaces).length);
+    }
+
+    addOutput() {
+        this.node.addOutput("Output" + Object.keys(this.node.outputInterfaces).length);
+    }
+
+    remove(name: string) {
+        this.node.removeInterface(name);
+    }
+
+    renameInput(from: string, to: string) {
+        this.node.removeInterface(from);
+        this.node.addInput(to);
+    }
+
+    renameOutput(from: string, to: string) {
+        this.node.removeInterface(from);
+        this.node.addOutput(to);
+    }
+
+}
+</script>
+
