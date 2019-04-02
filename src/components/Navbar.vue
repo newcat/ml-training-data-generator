f42b17c7e113fe516ae1bc2375a2abc9974b976b
newcat <freddy.wagner@web.de>
2019-04-02 10:14:21 +0200

    Added settings

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+nav.navbar.navbar-dark.bg-secondary.navbar-expand-lg
+    .navbar-brand ML Training Data Generator
+    button.navbar-toggler(type="button", data-toggle="collapse", data-target="#navbarCollapse")
+        span.navbar-toggler-icon
+    #navbarCollapse.collapse.navbar-collapse
+        ul.navbar-nav.mr-auto.ml-2
+            li.nav-item.dropdown
+                a#fileDropdown.nav-link.dropdown-toggle(role="button", data-toggle="dropdown") File
+                    .dropdown-menu
+                        a.dropdown-item(@click="$emit('load')", href="#") Load
+                        a.dropdown-item(@click="$emit('save')", href="#") Save
+            li.nav-item
+                a.nav-link(@click="$emit('calculate')", href="#") Calculate
+            li.nav-item
+                router-link.nav-link(:to="{ name: 'settings' }") Settings
+</template>
--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,11 @@ import { BaklavaVuePlugin } from "@baklavajs/plugin-renderer-vue";
 Vue.use(BaklavaVuePlugin);
 import "@baklavajs/plugin-renderer-vue/dist/styles.css";
 
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./customBootstrap.scss";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import router from "./router";
 
 new Vue({
+    router,
     render: (h) => h(App)
 }).$mount("#app");
